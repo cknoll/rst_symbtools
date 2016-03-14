11f157c40828f041b6de41f25ce033e81e3efbfc
cknoll <CarstenKnoll@gmx.de>
2016-03-14 23:49:58 +0100

    adaptions to run all tests

--- symbtools/tests/test_all.py
+++ symbtools/tests/test_all.py
@@ -9,16 +9,22 @@ import sys
 
 if 'all' in sys.argv:
     FLAG_all = True
-    sys.argv.remove('all')
 else:
     FLAG_all = False
 
 from test_core import *
 from test_modeltools import *
 from test_nctools import *
+from test_quick import *
+
+import test_core, test_modeltools, test_nctools, test_quick
 
     
 def main():
+    # remove command line args which should not be passed to the testframework
+    if 'all' in sys.argv:
+        sys.argv.remove('all')
+    
     unittest.main()
 
 # see also the skip_slow logic at the beginning of the file
